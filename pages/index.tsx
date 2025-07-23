ec55cc1d41a78414fa62f86bb3956f4e70d874f0
Jackson N <njihiajack7son@gmail.com>
2025-07-24 00:41:50 +0300

    Remove unused imports

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,14 +1,9 @@
 import Hero from '@/components/Hero'
-//import About from '@/components/About'
-//import Skills from '@/components/Skills'
-//import Projects from '@/components/Projects'
-//import Contact from '@/components/Contact'
 
 export default function Home() {
   return (
     <main>
       <Hero />
-      
     </main>
   )
 }
